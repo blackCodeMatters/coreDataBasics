838db166edc1b835bb74edb2c9a731fa54a6066a
Dustin M <Eleuthera@Dustins-MacBook-Air.local>
2016-02-21 09:07:11 -0500

    first commit

--- CoreDataBasics/ViewController.swift
+++ CoreDataBasics/ViewController.swift
@@ -7,12 +7,72 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        
+        newUser.setValue("Dustin", forKey: "username")
+        newUser.setValue("touchdown", forKey: "password")
+        
+        do {
+            
+            try context.save()
+            
+        } catch{
+            
+            print("an error ocurred")
+            
+        }
+        
+        
+
+        let request = NSFetchRequest(entityName: "Users")
+        
+        request.predicate = NSPredicate(format: "username = %@", "Dustin")
+        request.predicate = NSPredicate(format: "password =%@", "touchdown")
+        
+        request.returnsObjectsAsFaults = false //needed to make data accessible
+        
+        do {
+            
+            let results = try context.executeFetchRequest(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    //context.deleteObject(result) //used to delete an entry
+                    
+                    if let username = result.valueForKey("username") as? String {
+                        print(username)
+
+                    }
+                    
+                    /*
+                    print(result.valueForKey("username")!)
+                    print(result.valueForKey("password")!)
+                    */
+                }
+                
+            }
+            
+        } catch {
+            
+            print("uh oh")
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
